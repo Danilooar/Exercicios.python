d2278f8008b0a6dfa957c786125244cbfe966a28
Danilooar <luizdanilo13@gmail.com>
2024-04-13 23:37:35 -0300

    Novos exercicios

---
+++ Exercicio02.py
@@ -0,0 +1,43 @@
+"""EXERCICIO DE NUMERO 2 DO LIVRO FLUENT 
+1. Quantos segundos há em 42 minutos e 42 segundos?
+2. Quantas milhas há em 10 quilômetros? Dica: uma milha equivale a 1,61 quilômetro.
+3. Se você correr 10 quilômetros em 42 minutos e 42 segundos, qual é o seu passo
+médio (tempo por milha em minutos e segundos)? Qual é a sua velocidade média em
+milhas por hora?
+"""
+
+Um_minuto = 60
+
+Tempo= 420;42
+
+print( Um_minuto * Tempo );
+
+km = 10
+
+Milha = 1.609
+
+print(km/Milha)
+
+
+print("---------------------------------")
+
+Correu = 10
+
+Tempo = 42;42
+
+Passo = Tempo / Correu;
+
+
+print ("Distancia: " + str(Correu) + "km ")
+
+print("Tempo total: " + str(Tempo) + " minutos")
+
+print("Pace: " + str(Passo) + " Km")
+
+y=2 
+
+def produto (x,y):
+  return x*y
+x=10
+y+5
+print(produto(x,y))
